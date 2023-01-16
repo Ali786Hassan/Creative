989df4755eaf01f39d378cba418dc23d8b620d37
Ali786Hassan <alihassan.education@gmail.com>
2023-01-16 19:55:42 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={{flex:1,backgroundColor:'#fff',justifyContent:'center',alignItems:'center'}}>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
