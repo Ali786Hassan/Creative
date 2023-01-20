0cfef272fd3146c93839e79aad2c65ca4e6bfd42
Ali786Hassan <alihassan.education@gmail.com>
2023-01-20 23:52:58 +0500

    second

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Home from './App/Screens/Home';
-
+import Login from './App/Screens/Login';
 export default function App() {
   return (
-    <Home></Home>
+   <Login/>
   )
 }
 
