d2094bc57dd5c4ffe2a3cf738a51a473c2ae6936
Ali786Hassan <alihassan.education@gmail.com>
2023-01-22 18:28:25 +0500

    2nd

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -1,50 +1,50 @@
-import { View, Text,StyleSheet,TouchableOpacity,Image,FlatList, SafeAreaView } from 'react-native';
-import colors from '../Assets/Config/colors';
-import Sizes from '../Assets/Config/Sizes';
-import React from 'react'
+// import { View, Text,StyleSheet,TouchableOpacity,Image,FlatList, SafeAreaView } from 'react-native';
+// import colors from '../Assets/Config/colors';
+// import Sizes from '../Assets/Config/Sizes';
+// import React from 'react'
 
 
-const Home = () => {
+// const Home = () => {
 
-   function renderHeader() {
-     return (
-        <View style={{ flexDirection:'row',height:50}}>
-           <TouchableOpacity
-              style={{
-                width:50,
-                paddingLeft: 10,
-                justifyContent:'center'
-              }}
-           >
-              <Image
-                source={require('../Assets/Icon/nearby.png')}
-                resizeMode="contain"
-                style={{
-                  width:30,
-                  height:30
-                }}
-              />
-           </TouchableOpacity>
-        </View>
-     )
-   }
-  return (
-   <SafeAreaView style={styles.container}>
-       {renderHeader()}
-   </SafeAreaView>
-  )
-}
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: '#fff'
-  },
-  shadow:{
-    shadowColor:'#000',
-    shadowOffset: {
-      width:0,
-      height:3,
-    },
-  }
-})
-export default Home;
+//    function renderHeader() {
+//      return (
+//         <View style={{ flexDirection:'row',height:50}}>
+//            <TouchableOpacity
+//               style={{
+//                 width:50,
+//                 paddingLeft: 10,
+//                 justifyContent:'center'
+//               }}
+//            >
+//               <Image
+//                 source={require('../Assets/Icon/nearby.png')}
+//                 resizeMode="contain"
+//                 style={{
+//                   width:30,
+//                   height:30
+//                 }}
+//               />
+//            </TouchableOpacity>
+//         </View>
+//      )
+//    }
+//   return (
+//    <SafeAreaView style={styles.container}>
+//        {renderHeader()}
+//    </SafeAreaView>
+//   )
+// }
+// const styles = StyleSheet.create({
+//   container:{
+//     flex:1,
+//     backgroundColor: '#fff'
+//   },
+//   shadow:{
+//     shadowColor:'#000',
+//     shadowOffset: {
+//       width:0,
+//       height:3,
+//     },
+//   }
+// })
+// export default Home;
